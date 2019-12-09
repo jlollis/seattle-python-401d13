ab3eb32da6ed82adbb91c5b559888b4467bb7b8c
JB <jb.tellez@gmail.com>
2019-12-09 12:05:26 -0800

    updates sweet_tooth demo

--- class-06/demos/sweet-tooth/sweet_tooth.py
+++ class-06/demos/sweet-tooth/sweet_tooth.py
@@ -1,16 +1,29 @@
-class SweetTooth:
-    def __init__(self, sweets=[]):
-        self.sweets = sweets or ['snickers','gelato','raisinettes']
 
+class SweetTooth:
+    def __init__(self):
+        self.sweets = ['snickers','gelato','raisinettes']
 
     def feed(self, ingestible):
         if ingestible not in self.sweets:
-            raise NotSweetError('Sweets only!!!')
+            raise NotSweetError('Sweets only!')
 
-        # val = 1/0
+        # do other stuff
+        2/0 # breaking on purpose
 
         return True
 
 
 class NotSweetError(ValueError):
     pass
+
+if __name__ == "__main__":
+    st = SweetTooth()
+
+    try:
+        st.feed('celery')
+    except NotSweetError as nse: # good way
+        print('I tried to feed healthy!')
+        print(nse)
+    except:
+        print('uh ohs')
+
